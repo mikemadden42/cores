f757c90eea02aae2cef67b17b3b6051af8c7e2f6
Michael Madden <mikemadden42@icloud.com>
2020-04-13 21:41:16 -0500

    Use ansi_term to add color

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-extern crate num_cpus;
+use ansi_term::Colour::RGB;
+use num_cpus::get;
+use num_cpus::get_physical;
 
 fn main() {
-    let cores = num_cpus::get();
-    let cpus = num_cpus::get_physical();
-    println!("logical cores = {}", cores);
-    println!("physical cpus = {}", cpus);
+    let cores = get();
+    let cpus = get_physical();
+    println!("{} {}", RGB(112, 150, 75).paint("logical cores:"), RGB(90, 72, 98).paint(cores.to_string()));
+    println!("{} {}", RGB(112, 150, 75).paint("physical cores:"), RGB(90, 72, 98).paint(cpus.to_string()));
 }
