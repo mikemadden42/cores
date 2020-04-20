1cc7f9382dca1e406b472b5ddd30a9f6fd57b419
Michael Madden <mikemadden42@icloud.com>
2020-04-20 12:32:02 -0500

    Use non RGB colors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use ansi_term::Colour::RGB;
+use ansi_term::Colour::Blue;
+use ansi_term::Colour::Green;
 use num_cpus::get;
 use num_cpus::get_physical;
 
 fn main() {
     let cores = get();
     let cpus = get_physical();
-    println!("{} {}", RGB(112, 150, 75).paint("logical cores:"), RGB(90, 72, 98).paint(cores.to_string()));
-    println!("{} {}", RGB(112, 150, 75).paint("physical cores:"), RGB(90, 72, 98).paint(cpus.to_string()));
+    println!(
+        "{} {}",
+        Blue.paint("logical cores:"),
+        Green.paint(cores.to_string())
+    );
+    println!(
+        "{} {}",
+        Blue.paint("physical cores:"),
+        Green.paint(cpus.to_string())
+    );
 }
