2113b180f7bae6d206d38ba18624b97f47152502
Michael Madden <mikemadden42@icloud.com>
2022-09-17 14:22:26 -0500

    Fix Windows build issue after removing ansi_term.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use num_cpus::get_physical;
 fn main() {
     let cores = get();
     let cpus = get_physical();
-    #[cfg(target_os = "windows")]
-    let _enabled = ansi_term::enable_ansi_support();
     println!("logical cores: {}", cores);
     println!("physical cores: {}", cpus);
 }
