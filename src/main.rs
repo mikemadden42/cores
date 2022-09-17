4b95af22809b2b3c3ac3920a182f2a66b99997d7
Michael Madden <mikemadden42@icloud.com>
2022-09-17 14:12:16 -0500

    Remove ansi_term.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use ansi_term::Colour::Blue;
-use ansi_term::Colour::Green;
 use num_cpus::get;
 use num_cpus::get_physical;
 
@@ -8,14 +6,6 @@ fn main() {
     let cpus = get_physical();
     #[cfg(target_os = "windows")]
     let _enabled = ansi_term::enable_ansi_support();
-    println!(
-        "{} {}",
-        Blue.paint("logical cores:"),
-        Green.paint(cores.to_string())
-    );
-    println!(
-        "{} {}",
-        Blue.paint("physical cores:"),
-        Green.paint(cpus.to_string())
-    );
+    println!("logical cores: {}", cores);
+    println!("physical cores: {}", cpus);
 }
