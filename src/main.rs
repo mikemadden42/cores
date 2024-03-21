faa7b9437a971592339532a394716772dec7c31e
Michael Madden <mikemadden42@icloud.com>
2024-03-21 06:39:29 -0500

    Fix clippy issues.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use num_cpus::get_physical;
 fn main() {
     let cores = get();
     let cpus = get_physical();
-    println!("logical cores: {}", cores);
-    println!("physical cores: {}", cpus);
+    println!("logical cores: {cores}");
+    println!("physical cores: {cpus}");
 }
